ffb5af42883ac09061310487775ea6208231a092
20241204 <18745620828@189.cn>
2025-02-04 14:02:21 +0800

    Update revive.sh

--- revive.sh
+++ revive.sh
@@ -13,6 +13,31 @@
 #echo "host info:$HOSTS_JSON"
 # 使用 jq 提取 JSON 数组，并将其加载为 Bash 数组
 hosts_info=($(echo "${HOSTS_JSON}" | jq -c ".info[]"))
+tgsend() {
+  message_text=$1
+  #解析模式，可选HTML或Markdown
+  MODE='HTML'
+  #api接口
+  telegramBotToken=${TELEGRAM_TOKEN}
+  telegramBotUserId=${TELEGRAM_USERID}
+
+  URL="https://api.telegram.org/bot${telegramBotToken}/sendMessage"
+  if [[ -z ${telegramBotToken} ]]; then
+    echo "未配置TG推送"
+  else
+    res=$(timeout 20s curl -s -X POST $URL -d chat_id=${telegramBotUserId} -d parse_mode=${MODE} -d text="${message_text}")
+    if [ $? == 124 ]; then
+      echo 'TG_api请求超时,请检查网络是否重启完成并是否能够访问TG'
+      exit 1
+    fi
+    resSuccess=$(echo "$res" | jq -r ".ok")
+    if [[ $resSuccess = "true" ]]; then
+      echo "TG推送成功"
+    else
+      echo "TG推送失败，请检查TG机器人token和ID"
+    fi
+  fi
+}
 
 for info in "${hosts_info[@]}"; do
   user=$(echo $info | jq -r ".username")
@@ -20,15 +45,15 @@ for info in "${hosts_info[@]}"; do
   port=$(echo $info | jq -r ".port")
   pass=$(echo $info | jq -r ".password")
 
-  script="/home/$user/serv00-play/keepalive.sh autoupdate ${SENDTYPE} ${TELEGRAM_TOKEN} ${TELEGRAM_USERID} ${WXSENDKEY}"
-
+  script="uname -a && sleep 60 && ps aux && exit"
   output=$(sshpass -p "$pass" ssh -o StrictHostKeyChecking=no -p "$port" "$user@$host" "bash -s" <<< "$script")
 
   echo "output:$output"
-  if echo "$output" | grep -q "更新完毕"; then
+  if echo "$output" | grep -q "$host"; then
     echo "登录成功"
+    tgsend "主机:$host,用户名:$user,登录成功请检查!\n$output"
   else
     echo "登录失败"
-    ./tgsend.sh "主机:$host,用户名:$user,登录失败请检查!"
+    tgsend "主机:$host,用户名:$user,登录失败请检查!\n$output"
   fi
 done
